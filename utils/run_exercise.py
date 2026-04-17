fe9e97338863c9dae92c3c1c2ca11707c1b1903b
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2026-04-17 03:44:40 -0400

    Use SPDX lines for copyright holder and license info on Barefoot and ONF files That is, files with copyright notices in them that have Barefoot Networks or the Open Networking Foundation as the copyright holder.
    Signed-off-by: Andy Fingerhut <andy_fingerhut@alum.wustl.edu>

--- utils/run_exercise.py
+++ utils/run_exercise.py
@@ -1,25 +1,8 @@
 #!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2013 Barefoot Networks, Inc.
+#
 # SPDX-License-Identifier: Apache-2.0
-# Copyright 2013-present Barefoot Networks, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Adapted by Robert MacDavid (macdavid@cs.princeton.edu) from scripts found in
-# the p4app repository (https://github.com/p4lang/p4app)
-#
-# We encourage you to dissect this script to better understand the BMv2/Mininet
-# environment used by the P4 tutorial.
-#
 import argparse
 import json
 import os
