cd73f8f93dc2c66669fdf0d6108b4c7f3d35b3fe
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2026-07-22 23:28:45 -0400

    Add extra CI debug
    Signed-off-by: Andy Fingerhut <andy_fingerhut@alum.wustl.edu>

--- exercises/basic/runptf.sh
+++ exercises/basic/runptf.sh
@@ -43,6 +43,17 @@ echo ""
 echo "Started simple_switch_grpc. Waiting 2 seconds before starting PTF test..."
 sleep 2
 
+echo "P4_EXTRA_SUDO_OPTS=:${P4_EXTRA_SUDO_OPTS}:"
+echo "HOME=${HOME}"
+echo "PATH=${PATH}"
+echo "ls -la $HOME"
+echo "--------------------"
+ls -la $HOME
+echo "--------------------"
+echo "pip3 list -v"
+pip3 list -v
+echo "which ptf=:`which ptf`:"
+
 # ---- run tests ----
 sudo ${P4_EXTRA_SUDO_OPTS} `which ptf` \
     -i 0@veth1 \
